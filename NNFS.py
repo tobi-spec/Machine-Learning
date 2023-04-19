99286a6621df6b6880986063387e28dff86c1ad6
tobi8691 <post@tobias-hahn.eu>
2023-04-19 20:31:37 +0200

    formatting

--- NNFS.py
+++ NNFS.py
@@ -5,118 +5,47 @@ nnfs.init()
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
-        # Remember input values
         self.inputs = inputs
-        # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
-        # Backward pass
 
     def backward(self, dvalues):
-        # Gradients on parameters
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
-        # Gradient on values
         self.dinputs = np.dot(dvalues, self.weights.T)
 
 class Activation_ReLU:
-    # Forward pass
     def forward(self, inputs):
-        # Remember input values
         self.inputs = inputs
-        # Calculate output values from inputs
         self.output = np.maximum(0, inputs)
-        # Backward pass
+
     def backward(self, dvalues):
-        # Since we need to modify original variable,
-        # let’s make a copy of values first
         self.dinputs = dvalues.copy()
-        # Zero gradient where input values were negative
         self.dinputs[self.inputs <= 0] = 0
 
-# Softmax activation
 class Activation_Softmax:
-    # Forward pass
     def forward(self, inputs):
-        # Remember input values
         self.inputs = inputs
-        # Get unnormalized probabilities
-        exp_values = np.exp(inputs - np.max(inputs, axis=1,
-        keepdims=True))
-        # Normalize them for each sample
-        probabilities = exp_values / np.sum(exp_values, axis=1,
-        keepdims=True)
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
-        # Backward pass
 
     def backward(self, dvalues):
-        # Create uninitialized array
         self.dinputs = np.empty_like(dvalues)
-        # Enumerate outputs and gradients
         for index, (single_output, single_dvalues) in \
         enumerate(zip(self.output, dvalues)):
-            # Flatten output array
             single_output = single_output.reshape(-1, 1)
-            # Calculate Jacobian matrix of the output
-            jacobian_matrix = np.diagflat(single_output) - \
-            np.dot(single_output, single_output.T)
-            # Calculate sample-wise gradient
-            # and add it to the array of sample gradients
-            self.dinputs[index] = np.dot(jacobian_matrix,
-            single_dvalues)
-            
-# SGD optimizer
-class Optimizer_SGD:
-    # Initialize optimizer - set settings,
-    # learning rate of 1. is default for this optimizer
-    def __init__(self, learning_rate=1., decay=0., momentum=0.):
-        self.learning_rate = learning_rate
-        self.current_learning_rate = learning_rate
-        self.decay = decay
-        self.iterations = 0
-        self.momentum = momentum
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
 
-    # Call once before any parameter updates
-    def pre_update_params(self):
-        if self.decay:
-            self.current_learning_rate = self.learning_rate * \
-            (1. / (1. + self.decay * self.iterations))
-
-    # Update parameters
-    def update_params(self, layer):
-        if self.momentum:
-            if not hasattr(layer, 'weight_momentums'):
-                layer.weight_momentums = np.zeros_like(layer.weights)
-                layer.bias_momentums = np.zeros_like(layer.biases)
- 
-            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
-            layer.weight_momentums = weight_updates
-
-            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
-            layer.bias_momentums = bias_updates
- 
-        else:
-            weight_updates = -self.current_learning_rate * layer.dweights
-            bias_updates = -self.current_learning_rate * layer.dbiases
-
-        layer.weights += weight_updates
-        layer.biases += bias_updates
-
-    # Call once after any parameter updates
-    def post_update_params(self):
-        self.iterations += 1
 
 class Loss:
     def calculate(self, output, y):
-        # Calculate sample losses
         sample_losses = self.forward(output, y)
-        # Calculate mean loss
         data_loss = np.mean(sample_losses)
-        # Return loss
         return data_loss
 
 class Loss_CategoricalCrossentropy(Loss):
@@ -125,16 +54,11 @@ class Loss_CategoricalCrossentropy(Loss):
         y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
 
         if len(y_true.shape) == 1:
-            correct_confidences = y_pred_clipped[
-            range(samples),
-            y_true
-            ]
+            correct_confidences = y_pred_clipped[range(samples),y_true]
       
         elif len(y_true.shape) == 2:
-            correct_confidences = np.sum(
-            y_pred_clipped * y_true,
-            axis=1
-            )
+            correct_confidences = np.sum(y_pred_clipped * y_true,axis=1)
+
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
 
@@ -165,6 +89,40 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         self.dinputs = dvalues.copy()
         self.dinputs[range(samples), y_true] -= 1
         self.dinputs = self.dinputs / samples
+            
+class Optimizer_SGD:
+    def __init__(self, learning_rate=1., decay=0., momentum=0.):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.momentum = momentum
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+    def update_params(self, layer):
+        if self.momentum:
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+ 
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+ 
+        else:
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+
+        layer.weights += weight_updates
+        layer.biases += bias_updates
+
+    def post_update_params(self):
+        self.iterations += 1
 
 X, y = spiral_data(samples=100, classes=3)
 
