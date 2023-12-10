df91d388c8d697ac7306909e889e113103628728
tobias.weiland <tobias.weiland@7p-group.com>
2023-12-10 22:07:58 +0100

    chnage keras digit classification loss function to Categorical crossentropy

--- Computervision/DigitClassification/digit_classification_keras.py
+++ Computervision/DigitClassification/digit_classification_keras.py
@@ -9,16 +9,15 @@ start = timeit.default_timer()
 class MNISTDataLoader:
     def __init__(self):
         self.train_images = idx2numpy.convert_from_file("./MNIST/train-images.idx3-ubyte")
-        self.train_labels = idx2numpy.convert_from_file("./MNIST/train-labels.idx1-ubyte")
+        self.train_labels = tf.keras.utils.to_categorical(idx2numpy.convert_from_file("./MNIST/train-labels.idx1-ubyte"))
         self.test_images = idx2numpy.convert_from_file("./MNIST/t10k-images.idx3-ubyte")
-        self.test_labels = idx2numpy.convert_from_file("./MNIST/t10k-labels.idx1-ubyte")
+        self.test_labels = tf.keras.utils.to_categorical(idx2numpy.convert_from_file("./MNIST/t10k-labels.idx1-ubyte"))
 
     def info(self):
         print("Number of images for training: ", len(self.train_images), self.train_images.shape)
         print("Number of labels for training: ", len(self.train_labels), self.train_labels.shape)
         print("Number of images for testing: ", len(self.test_images), self.test_images.shape)
         print("Number of labels for testing: ", len(self.test_labels), self.test_labels.shape)
-        print("Labels: ", set(self.train_labels))
 
     def display_train_image(self, position):
         plt.imshow(self.train_images[position], cmap=plt.cm.binary)
@@ -36,7 +35,7 @@ model = tf.keras.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 model.add(tf.keras.layers.Dense(units=128, activation="relu"))
 model.add(tf.keras.layers.Dense(units=10, activation="softmax"))
-model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss=tf.keras.losses.CategoricalCrossentropy())
 model.fit(mnist.train_images, mnist.train_labels, epochs = 15, batch_size=32)
 
 prediction = model.predict(mnist.test_images[6758:6759])
