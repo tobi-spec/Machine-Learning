456a945cefa8b94b5f5ef4104634b7fab31089a0
tobias.weiland <tobias.weiland@7p-group.com>
2023-12-15 16:35:47 +0100

    refactor init method of model

--- Computervision/DigitClassification/digit_classification_pytorch.py
+++ Computervision/DigitClassification/digit_classification_pytorch.py
@@ -26,24 +26,22 @@ class MNISTDataset(Dataset):
 class MNISTClassificationModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear1 = nn.Linear(784, 128)
-        self.activation1 = nn.ReLU()
-        self.linear2 = nn.Linear(128, 10)
-        self.activation2 = nn.Softmax(dim=1)
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(784, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+            nn.Softmax(dim=1),
+        )
         self.loss_function = nn.CrossEntropyLoss()
         self.optimizer_function = torch.optim.Adam(self.parameters(), lr=0.001)
 
-        torch.nn.init.normal_(self.linear1.weight, mean=0.0, std=1.0)
-        torch.nn.init.normal_(self.linear2.weight, mean=0.0, std=1.0)
+    def init_weights(self):
+        if isinstance(self.model, nn.Linear):
+            torch.nn.init.normal_(self.model.weight, mean=0.0, std=1.0)
 
     def forward(self, inputs):
-        inputs = self.flatten(inputs)
-        inputs = self.linear1(inputs)
-        inputs = self.activation1(inputs)
-        inputs = self.linear2(inputs)
-        inputs = self.activation2(inputs)
-        return inputs
+        return self.model(inputs)
 
     def backward(self, train_loader, epoch, num_epochs):
         self.train()
@@ -54,7 +52,6 @@ class MNISTClassificationModel(nn.Module):
             loss = self.loss_function(prediction, y_values)
             loss.backward()
             self.optimizer_function.step()
-
         print(f"Epoch [{epoch + 1}/{num_epochs}] | Train Loss: {loss.item():.4f}")
 
     def validate(self, val_loader):
@@ -64,7 +61,6 @@ class MNISTClassificationModel(nn.Module):
             for x_values, y_values in val_loader:
                 prediction = self.forward(x_values)
                 loss = self.loss_function(prediction, y_values)
-
         print(f'Validation Loss: {loss.item():.4f}')
 
 
@@ -75,7 +71,7 @@ train_loader = DataLoader(dataset=mnist_training, batch_size=32, shuffle=True)
 test_loader = DataLoader(dataset=mnist_test, batch_size=32, shuffle=False)
 
 model = MNISTClassificationModel()
-
+model.init_weights()
 num_epochs = 15
 for epoch in range(num_epochs):
     model.backward(train_loader, epoch, num_epochs)
@@ -85,7 +81,4 @@ prediction = model.forward(torch.Tensor(mnist_test.images[786]).view(1, -1))
 stop = timeit.default_timer()
 print("predicted number: ", np.argmax(prediction.detach().numpy()))
 print("correct number ", mnist_test.labels[786])
-print(f"run time[s]: {stop-start}")
-
-
-
+print(f"run time[s]: {stop - start}")
