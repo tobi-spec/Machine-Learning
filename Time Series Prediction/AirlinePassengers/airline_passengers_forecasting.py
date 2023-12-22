39f1013553ccc2bbae7341b2b9c5e5ef729df537
tobias.weiland <tobias.weiland@7p-group.com>
2023-12-22 16:39:43 +0100

    add basic implementation for timeseries forecast

---
+++ Time Series Prediction/AirlinePassengers/airline_passengers_forecasting.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# https://machinelearningmastery.com/time-series-prediction-with-deep-learning-in-python-with-keras/
+
+class AirlinePassengersDataSet:
+    def __init__(self):
+        self.data = pd.read_csv("./AirlinePassengers.csv", sep=";")
+        self.month = self.data["Month"]
+        self.passengers = self.data["Passengers"]
+        self.passengers_plus_1 = None
+
+    def create_passengers_plus_1(self):
+        series1 = self.passengers[1:]
+        series2 = pd.Series([0], index=[144])
+        self.passengers_plus_1 = pd.concat([series1, series2])
+
+    def add_passengers_plus_1(self):
+        self.data["Passengers+1"] = self.passengers_plus_1.reset_index(drop=True)
+        self.data.drop(self.data.tail(1).index, inplace=True)
+
+    def get_train_test(self):
+        train = self.data[0:107]
+        test = self.data[107:142]
+        return train, test
+
+
+timeseries = AirlinePassengersDataSet()
+timeseries.create_passengers_plus_1()
+timeseries.add_passengers_plus_1()
+
+train, test = timeseries.get_train_test()
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units=8, activation="relu"))
+model.add(tf.keras.layers.Dense(units=1))
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='mean_squared_error')
+model.fit(train["Passengers"], train["Passengers+1"], epochs=25, batch_size=1)
+
+predictions = []
+for element in test["Passengers"]:
+    prediction = model.predict([element])
+    predictions.append(prediction[0])
+
+plt.plot(train["Month"], train["Passengers"])
+plt.plot(test["Month"], predictions)
+plt.show()
