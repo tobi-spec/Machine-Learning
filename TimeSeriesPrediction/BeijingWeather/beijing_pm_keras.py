718f07086248720ea28f92cdb65c32ee9e2b3fa4
Tobias Weiland <post@tobias-weiland.eu>
2024-01-07 16:40:31 +0100

    add data preparation for Beijin wather data

--- TimeSeriesPrediction/BeijingWeather/beijing_pm_keras.py
+++ TimeSeriesPrediction/BeijingWeather/beijing_pm_keras.py
@@ -2,11 +2,18 @@ import pandas as pd
 from datetime import datetime
 
 dataset = pd.read_csv(
-    "BeijingParticulateMatter.csv",
+    filepath_or_buffer="BeijingParticulateMatter.csv",
     delimiter=";",
-    parse_dates=True,
+    index_col=0,
+    parse_dates=[[1 ,2 , 3, 4]],
     date_format='%Y %m %d %H')
 
+dataset.drop(["No"], axis=1, inplace=True)
+dataset.columns = ['pollution', 'dew', 'temperature', 'pressure', 'wind_direction', 'wind_speed', 'snow', 'rain']
+dataset.index.name = 'date'
+dataset["pollution"] = dataset["pollution"].fillna(0)
+dataset = dataset[24:]
+dataset.to_csv("./pollution.csv")
 print(dataset)
 
 #https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
