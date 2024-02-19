27aadd9cfa4235afc446a9e987b020ce2fe4e7b5
Tobias Weiland <post@tobias-weiland.eu>
2024-02-19 19:08:23 +0100

    extract one-step forecast in function

--- TimeSeriesPrediction/2_UnivariantTimeSeriesForecast/airline_passengers_forecasting_keras.py
+++ TimeSeriesPrediction/2_UnivariantTimeSeriesForecast/airline_passengers_forecasting_keras.py
@@ -35,14 +35,17 @@ model.fit(train.loc[:, "Passengers"], train.loc[:, "Passengers+1"], epochs=25, b
 predictions = model.predict(test.loc[:, "Passengers"])
 test["Predictions"] = predictions
 
-current_value = train.iloc[-1:, 1]
-one_step_ahead_forecast = list()
-for element in range(0, len(test)):
-    prediction = model.predict([current_value])
-    one_step_ahead_forecast.append(prediction[0][0])
-    current_value = prediction[0]
-test["one_step_prediction"] = one_step_ahead_forecast
 
+def one_step_ahead_forecast(current_value):
+    one_step_ahead_forecast = list()
+    for element in range(0, len(test)):
+        prediction = model.predict([current_value])
+        one_step_ahead_forecast.append(prediction[0][0])
+        current_value = prediction[0]
+    return one_step_ahead_forecast
+
+current_value = train.iloc[-1:, 1]
+test["one_step_prediction"] = one_step_ahead_forecast(current_value)
 test.to_csv("./AirlinePassengersResultsKeras.csv")
 
 plt.plot(train["Month"], train["Passengers"], color="green", label="training")
