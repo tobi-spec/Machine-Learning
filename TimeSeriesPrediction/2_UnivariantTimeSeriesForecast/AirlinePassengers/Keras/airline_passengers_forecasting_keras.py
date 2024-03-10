8d58848b80441c093b9ccc61f52b4fa996a56d66
Tobias Weiland <post@tobias-weiland.eu>
2024-03-10 12:20:30 +0100

    replace create_timeseries and add normalisation in alir passengers forecasting keras

--- TimeSeriesPrediction/2_UnivariantTimeSeriesForecast/AirlinePassengers/Keras/airline_passengers_forecasting_keras.py
+++ TimeSeriesPrediction/2_UnivariantTimeSeriesForecast/AirlinePassengers/Keras/airline_passengers_forecasting_keras.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 # Normalizer, LSTM, LSTM-Architecture, Beschreibung
 
@@ -9,44 +10,38 @@ import matplotlib.pyplot as plt
 class AirlinePassengersDataSet:
     def __init__(self):
         self.data = pd.read_csv("../AirlinePassengers.csv", sep=";")
-        self.data.drop(["Month"], inplace=True, axis=1)
         self.threshold = 107
 
-    def create_targets(self):
-        self.data["Passengers+1"] = self.data.loc[:, "Passengers"].shift(-1)
-        self.data = self.data[:-2]
+    def get_train_data(self):
+        data = self.data.loc[0:self.threshold, "Passengers"].reset_index(drop=True)
+        return pd.DataFrame(data)
 
-    def get_train_inputs(self):
-        return self.data.loc[0:self.threshold, "Passengers"].reset_index(drop=True)
-
-    def get_train_targets(self):
-        return self.data.loc[0:self.threshold, "Passengers+1"].reset_index(drop=True)
-
-    def get_test_inputs(self):
-        return self.data.loc[self.threshold:142, "Passengers"].reset_index(drop=True)
-
-    def get_test_targets(self):
-        return self.data.loc[self.threshold:142, "Passengers+1"].reset_index(drop=True)
+    def get_test_data(self):
+        data = self.data.loc[self.threshold:142, "Passengers"].reset_index(drop=True)
+        return pd.DataFrame(data)
 
 
 airlinePassengers = AirlinePassengersDataSet()
-airlinePassengers.create_targets()
+scaler = MinMaxScaler(feature_range=(0, 1))
+train = scaler.fit_transform(airlinePassengers.get_train_data())
+test = scaler.fit_transform(airlinePassengers.get_test_data())
 
 
-def create_timeseries(inputs, targets, span):
-    timeseries = list()
-    for i in range(span, len(inputs)):
-        timeseries.append(inputs.loc[i - span:i])
-    timeseries = np.array(timeseries)
-    targets = targets[span:]
-    return timeseries, targets
+def create_timeseries(data, previous=1):
+    inputs, targets = list(), list()
+    for i in range(len(data)-previous-1):
+        a = data[i: (i+previous), 0]
+        inputs.append(a)
+        targets.append(data[i + previous, 0])
+    return np.array(inputs), np.array(targets)
 
-size_of_timeseries = 15
-train_input_timeseries, train_targets = create_timeseries(airlinePassengers.get_train_inputs(),
-                                                          airlinePassengers.get_train_targets(), size_of_timeseries)
 
-test_input_timeseries, test_targets = create_timeseries(airlinePassengers.get_test_inputs(),
-                                                        airlinePassengers.get_test_targets(), size_of_timeseries)
+lookback = 15
+train_inputs, train_targets = create_timeseries(train, lookback)
+test_inputs, test_targets = create_timeseries(test, lookback)
+
+train_inputs = np.reshape(train_inputs, (train_inputs.shape[0], 1, train_inputs.shape[1]))
+test_inputs = np.reshape(test_inputs, (test_inputs.shape[0], 1, test_inputs.shape[1]))
 
 
 def create_FFN(inputs, targets):
@@ -58,7 +53,7 @@ def create_FFN(inputs, targets):
     return model
 
 
-ffn = create_FFN(train_input_timeseries, train_targets)
+ffn = create_FFN(train_inputs, train_targets)
 
 
 def validation_forecast(model, inputs):
@@ -67,8 +62,8 @@ def validation_forecast(model, inputs):
 
 
 validation = pd.DataFrame()
-validation["true"] = test_targets.shift(1)
-validation["validation"] = validation_forecast(ffn, test_input_timeseries)
+validation["true"] = scaler.inverse_transform([test_targets]).flatten()
+validation["validation"] = scaler.inverse_transform([validation_forecast(ffn, test_inputs)]).flatten()
 validation.index += airlinePassengers.threshold
 
 
@@ -83,15 +78,14 @@ def one_step_ahead_forecast(model, current_value, number_of_predictions):
     return one_step_ahead_forecast
 
 
-start_value = train_input_timeseries[-1]
-start_value_reshaped = start_value.reshape(1, start_value.shape[0])
+start_value = train_inputs[-1]
 number_of_predictions = 40
 
 prediction = pd.DataFrame()
-prediction["one_step_prediction"] = one_step_ahead_forecast(ffn, start_value_reshaped, number_of_predictions)
+prediction["one_step_prediction"] = scaler.inverse_transform([one_step_ahead_forecast(ffn, start_value, number_of_predictions)]).flatten()
 prediction.index += airlinePassengers.threshold
 
-plt.plot(airlinePassengers.get_train_inputs(), color="green", label="training")
+plt.plot(airlinePassengers.get_train_data(), color="green", label="training")
 plt.plot(validation["true"], color="red", label="true")
 plt.plot(validation["validation"], color="blue", label="validation")
 plt.plot(prediction["one_step_prediction"], color="orange", label="one_step_prediction")
