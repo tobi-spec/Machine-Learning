ad77f095e2256332f47927de08032084a06350f0
Tobias Weiland <post@tobias-weiland.eu>
2024-03-20 22:28:42 +0100

    refactor airline_passengers_forecasting_pytorch to use real time series

--- TimeSeriesPrediction/2_UnivariantTimeSeriesForecast/AirlinePassengers/Pytorch/airline_passengers_forecasting_pytorch.py
+++ TimeSeriesPrediction/2_UnivariantTimeSeriesForecast/AirlinePassengers/Pytorch/airline_passengers_forecasting_pytorch.py
@@ -1,53 +1,60 @@
 import torch
 import torch.nn as nn
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import MinMaxScaler
 
 
 class AirlinePassengersPreparer:
     def __init__(self):
         self.data = pd.read_csv("../AirlinePassengers.csv", sep=";")
-        self.month = self.data.loc[:, "Month"]
-        self.passengers = self.data.loc[:, "Passengers"]
-        self.passengers_plus_1 = None
+        self.threshold = 107
 
-    def create_forecast_sequence(self):
-        self.passengers_plus_1 = self.passengers.shift(-1)
-        self.passengers_plus_1 = self.passengers_plus_1.iloc[:-1]
-        self.data["Passengers+1"] = self.passengers_plus_1
-        self.passengers = self.passengers.iloc[:-1]
+    def get_train_data(self):
+        data = self.data.loc[0:self.threshold, "Passengers"].reset_index(drop=True)
+        return pd.DataFrame(data)
 
-    def get_train_test(self):
-        train = self.data[0:107]
-        test = self.data[107:142]
-        return train, test
+    def get_test_data(self):
+        data = self.data.loc[self.threshold:142, "Passengers"].reset_index(drop=True)
+        return pd.DataFrame(data)
 
 
-class AirlinePassengersDataset(Dataset):
-    def __init__(self, passengers, passengers_plus_1):
-        self.passengers = torch.from_numpy(passengers.astype("float32"))
-        self.passengers_plus_1 = torch.from_numpy(passengers_plus_1.astype("float32"))
+airline_passengers = AirlinePassengersPreparer()
+scaler = MinMaxScaler(feature_range=(0, 1))
+train = scaler.fit_transform(airline_passengers.get_train_data())
+test = scaler.fit_transform(airline_passengers.get_test_data())
 
-    def __len__(self):
-        return len(self.passengers)
 
-    def __getitem__(self, index):
-        return self.passengers[index], self.passengers_plus_1[index]
+def create_timeseries(data, previous=1):
+    inputs, targets = list(), list()
+    for i in range(len(data)-previous-1):
+        a = data[i: (i+previous), 0]
+        inputs.append(a)
+        targets.append([data[i + previous, 0]])
+    return torch.tensor(inputs, dtype=torch.float), torch.tensor(targets, dtype=torch.float)
+
+
+lookback = 30
+train_inputs, train_targets = create_timeseries(train, lookback)
+test_inputs, test_targets = create_timeseries(test, lookback)
+
+train_loader = DataLoader(dataset=TensorDataset(train_inputs, train_targets), batch_size=1, shuffle=False)
+test_loader = DataLoader(dataset=TensorDataset(test_inputs, test_targets), batch_size=1, shuffle=False)
 
 
 class AirlinePassengersModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear1 = nn.Linear(1, 8)
-        self.activation = nn.ReLU()
-        self.linear2 = nn.Linear(8, 1)
+        #self.lstm = nn.LSTM(input_size=1, hidden_size=50, num_layers=1)
+        self.linear1 = nn.Linear(30, 50)
+        self.linear2 = nn.Linear(50, 1)
         self.loss_function = nn.MSELoss()
-        self.optimizer_function = torch.optim.Adam(self.parameters(), lr=0.001)
+        self.optimizer_function = torch.optim.Adam(self.parameters(), lr=0.0001)
 
     def forward(self, inputs):
         inputs = self.linear1(inputs)
-        inputs = self.activation(inputs)
         inputs = self.linear2(inputs)
         return inputs
 
@@ -63,7 +70,8 @@ class AirlinePassengersModel(nn.Module):
             self.optimizer_function.zero_grad()
             cumulative_loss += loss.item()
 
-        print(f"Epoch [{epoch + 1}/{num_epochs}] | Train Loss: {cumulative_loss / len(train_loader):.4f}")
+        print(f"Epoch [{epoch + 1}/{num_epochs}]")
+        print(f"Train Loss: {cumulative_loss / len(train_loader):.4f}")
 
     def validate(self, val_loader):
         self.eval()
@@ -77,33 +85,25 @@ class AirlinePassengersModel(nn.Module):
         print(f'Validation Loss: {loss / len(val_loader):.4f}')
 
 
-timeseries = AirlinePassengersPreparer()
-timeseries.create_forecast_sequence()
-train, test = timeseries.get_train_test()
-
-train_dataset = AirlinePassengersDataset(train.loc[:, "Passengers"].to_numpy(), train.loc[:, "Passengers+1"].to_numpy())
-train_loader = DataLoader(dataset=train_dataset, batch_size=1, shuffle=False)
-
-test_dataset = AirlinePassengersDataset(test.loc[:, "Passengers"].to_numpy(), test.loc[:, "Passengers+1"].to_numpy())
-test_loader = DataLoader(dataset=test_dataset, batch_size=1, shuffle=False)
-
 model = AirlinePassengersModel()
-num_epochs = 25
+num_epochs = 50
 for epoch in range(num_epochs):
     model.backward(train_loader, epoch, num_epochs)
     model.validate(test_loader)
 
 predictions = []
-for element in test.loc[:, "Passengers"]:
-    prediction = model(torch.Tensor([element]))
+for element in test_inputs:
+    prediction = model(torch.Tensor(element))
     predictions.append(prediction.item())
-test["Predictions"] = predictions
+print(predictions)
 
-test.drop(["Passengers+1"], axis=1).to_csv("./AirlinePassengersResultsPytorch.csv")
+validation = pd.DataFrame()
+validation["Predictions"] = scaler.inverse_transform([predictions]).flatten()
+validation.index += airline_passengers.threshold
 
-plt.plot(train["Month"], train["Passengers"], color="green", label="training")
-plt.plot(test["Month"], test["Predictions"], color="red", label="prediction")
-plt.plot(test["Month"], test["Passengers"], color="blue", label="test")
+plt.plot(airline_passengers.get_train_data(), color="green", label="training")
+plt.plot(airline_passengers.get_test_data(), color="red", label="prediction")
+plt.plot(validation["Predictions"], color="blue", label="test")
 plt.title("airline passengers prediction")
 plt.xlabel("Time[Month]")
 plt.ylabel("Passengers[x1000]")
