b183aef7b6b658e646ec54d9764c2528c7d4635d
Tobias Weiland <post@tobias-weiland.eu>
2024-04-02 21:06:34 +0200

    increase size of plot for airline_passengers_visual.py

--- TimeSeriesPrediction/2_UnivariatTimeSeriesForecast/AirlinePassengers/airline_passengers_visual.py
+++ TimeSeriesPrediction/2_UnivariatTimeSeriesForecast/AirlinePassengers/airline_passengers_visual.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("./AirlinePassengers.csv", sep=";")
 
 plt.plot(data["Passengers"])
-plt.xticks(range(0, 150, 20))
-plt.yticks(range(0, 1000, 100))
+plt.xticks(range(0, 220, 20))
+plt.yticks(range(0, 1200, 100))
 plt.savefig("./airlinePassengers.png")
