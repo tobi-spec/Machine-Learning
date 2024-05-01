d17f34d7e48b84d1ecff9a8aafbe9890fa35ba93
Tobias Weiland <post@tobias-weiland.eu>
2024-05-01 21:35:26 +0200

    remove dense layer from air passengers keras feed forward network

--- TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/FF/airline_passengers_keras_ff.py
+++ TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/FF/airline_passengers_keras_ff.py
@@ -74,11 +74,6 @@ class FeedForwardModel(Model):
                                             kernel_initializer=initializers.RandomNormal(stddev=0.01),
                                             bias_initializer=initializers.Zeros()
                                             )
-        self.dense2 = layers.Dense(units=50,
-                                            activation="relu",
-                                            kernel_initializer=initializers.RandomNormal(stddev=0.01),
-                                            bias_initializer=initializers.Zeros())
-
         self.dense3 = layers.Dense(units=1,
                                             activation="relu",
                                             kernel_initializer=initializers.RandomNormal(stddev=0.01),
@@ -86,7 +81,6 @@ class FeedForwardModel(Model):
 
     def call(self, inputs):
         x = self.dense1(inputs)
-        x = self.dense2(x)
         x = self.dense3(x)
         return x
 
