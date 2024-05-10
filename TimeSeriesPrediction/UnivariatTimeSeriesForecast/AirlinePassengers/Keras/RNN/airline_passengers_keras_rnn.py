2f0df0efea9d8476a0e2de7527341ec4c9761fc2
Tobias Weiland <post@tobias-weiland.eu>
2024-05-10 17:21:06 +0200

    implement keras  rnn model for airline passengers

---
+++ TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/RNN/airline_passengers_keras_rnn.py
@@ -0,0 +1,22 @@
+from keras import Model, layers, initializers
+from airline_passengers_rnn_workflow import workflow
+
+
+class RNNModel(Model):
+    def __init__(self):
+        super().__init__()
+        self.lstm = layers.SimpleRNN(units=50,)
+        self.dense1 = layers.Dense(units=1,
+                                            activation="relu",
+                                            kernel_initializer=initializers.RandomNormal(stddev=0.01),
+                                            bias_initializer=initializers.Zeros())
+
+    def call(self, inputs):
+        x = self.lstm(inputs)
+        x = self.dense1(x)
+        return x
+
+
+if __name__ == "__main__":
+    rnn_model = RNNModel()
+    workflow(rnn_model)
