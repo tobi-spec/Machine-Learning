a8a2a75a72a5fe329550e1951a199719088fd8e2
Tobias Weiland <post@tobias-weiland.eu>
2024-05-10 19:36:20 +0200

    move workflow of keras ff model into seperate folder

--- TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/FF/airline_passengers_keras_ff.py
+++ TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/FF/airline_passengers_keras_ff.py
@@ -1,57 +1,5 @@
-from keras import Model, layers, optimizers, initializers
-import matplotlib.pyplot as plt
-from TimeSeriesPrediction.UnivariatTimeSeriesForecast.AirlinePassengers.airline_passengers_utilities import *
-
-EPOCHS = 1000
-LEARNING_RATE = 0.0001
-BATCH_SIZE = 1
-LOOK_BACK = 30
-PREDICTION_START = -1
-NUMBER_OF_PREDICTIONS = 80
-
-
-def main():
-    airline_passengers = AirlinePassengersDataSet()
-    train = airline_passengers.get_train_data()
-    test = airline_passengers.get_test_data()
-
-    train_inputs, train_targets = TimeSeriesGenerator(train, LOOK_BACK).create_timeseries()
-    test_inputs, test_targets = TimeSeriesGenerator(test, LOOK_BACK).create_timeseries()
-
-    model = FeedForwardModel()
-    model.compile(optimizer=optimizers.Adam(LEARNING_RATE), loss='mean_squared_error')
-    model.fit(train_inputs, train_targets, epochs=EPOCHS, batch_size=BATCH_SIZE)
-
-    validation_results = validation_forecast(model, test_inputs)
-
-    validation = pd.DataFrame()
-    validation["validation"] = validation_results
-    validation.index += airline_passengers.threshold + LOOK_BACK
-
-    start_index = PREDICTION_START
-    start_value = train_inputs[start_index]
-    start_value_reshaped = start_value.reshape(1, start_value.shape[0])
-    prediction_results = Forecaster(model,
-                                    start_value_reshaped,
-                                    NUMBER_OF_PREDICTIONS,
-                                    NeuronalNetworkTypes.FEED_FORWARD).one_step_ahead()
-
-    prediction = pd.DataFrame()
-    prediction["one_step_prediction"] = prediction_results
-    prediction.index += airline_passengers.threshold + start_index
-
-    plt.plot(airline_passengers.data["Passengers"], color="red", label="dataset")
-    plt.plot(airline_passengers.get_train_data(), color="green", label="training")
-    plt.plot(validation["validation"], color="blue", label="validation")
-    plt.plot(prediction["one_step_prediction"], color="orange", label="one_step_prediction")
-    plt.title("airline passengers prediction FF")
-    plt.xlabel("Time[Month]")
-    plt.ylabel("Passengers[x1000]")
-    plt.xticks(range(0, 200, 20))
-    plt.yticks(range(0, 1000, 100))
-    plt.legend(loc="upper left")
-    plt.savefig("./airlinePassengers_keras_ff.png")
-    plt.show()
+from keras import Model, layers, initializers
+from airline_passengers_basic_workflow import workflow
 
 
 class FeedForwardModel(Model):
@@ -80,4 +28,5 @@ class FeedForwardModel(Model):
 
 
 if __name__ == "__main__":
-    main()
+    model = FeedForwardModel()
+    workflow(model)
