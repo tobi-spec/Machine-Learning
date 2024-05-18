d8c20a28ce3e6a780b8b6f7434768758599a660f
tobias.weiland <tobias.weiland@7p-group.com>
2024-05-18 08:19:43 +0200

    remove rnn workflow

--- TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/models/airline_passengers_keras_rnn.py
+++ TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/models/airline_passengers_keras_rnn.py
@@ -1,5 +1,5 @@
 from keras import Model, layers, initializers
-from TimeSeriesPrediction.UnivariatTimeSeriesForecast.AirlinePassengers.Keras.workflows.airline_passengers_rnn_workflow import workflow
+from TimeSeriesPrediction.UnivariatTimeSeriesForecast.AirlinePassengers.Keras.workflows.airline_passengers_lstm_workflow import workflow
 
 
 class RNNModel(Model):
