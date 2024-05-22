6115dbc10df4a1ce26bb6575588db0f016927aed
Tobias Weiland <post@tobias-weiland.eu>
2024-05-22 19:45:17 +0200

    simplify seq2seq keras model

--- TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/models/airline_passengers_keras_seq2seq.py
+++ TimeSeriesPrediction/UnivariatTimeSeriesForecast/AirlinePassengers/Keras/models/airline_passengers_keras_seq2seq.py
@@ -5,13 +5,11 @@ from TimeSeriesPrediction.UnivariatTimeSeriesForecast.AirlinePassengers.Keras.wo
 
 def seq2seq_model():
     encoder_input = Input(shape=(1, 30))
-
-    encoder_lstm = LSTM(128, return_sequences=True)(encoder_input)
-    encoder_lstm, state_h, state_c = LSTM(128, return_state=True)(encoder_lstm)
+    encoder_lstm, state_h, state_c = LSTM(50, return_state=True)(encoder_input)
     encoder_states = [state_h, state_c]
 
     decoder_input = Input(shape=(1, 3))
-    decoder_lstm = LSTM(128, return_sequences=True, return_state=True)
+    decoder_lstm = LSTM(50, return_sequences=True, return_state=True)
     decoder_output, _, _ = decoder_lstm(decoder_input, initial_state=encoder_states)
     decoder_output = Dense(1)(decoder_output)
 
