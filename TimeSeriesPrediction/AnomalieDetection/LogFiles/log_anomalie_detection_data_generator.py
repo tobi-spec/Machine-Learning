24b3d3e93d7836f7c65817534ed36d37aa055f33
Tobias Weiland <post@tobias-weiland.eu>
2024-07-18 20:27:16 +0200

    fix bug in create_logfile

--- TimeSeriesPrediction/AnomalieDetection/LogFiles/log_anomalie_detection_data_generator.py
+++ TimeSeriesPrediction/AnomalieDetection/LogFiles/log_anomalie_detection_data_generator.py
@@ -7,24 +7,25 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 
-number_of_changes = np.random.randint(low=3, high=13)
-positions_of_changes = np.random.randint(low=1, high=51, size=number_of_changes)
+
+def create_logfile(logfile):
+    number_of_file = len(listdir("./data/correct"))
+    number_of_changes = np.random.randint(low=3, high=13)
+    positions_of_changes = np.random.randint(low=1, high=49, size=number_of_changes)
+    for position in positions_of_changes:
+        swap1 = logfile.iloc[position, 1:4]
+        swap2 = logfile.iloc[position+1, 1:4]
+
+        temp = swap1.copy()
+        logfile.iloc[position, 1:4] = swap2
+        logfile.iloc[position+1, 1:4] = temp
+
+    logfile.to_csv(f'./data/correct/log{number_of_file}.txt', index=False, header=False)
 
 
-number_of_file = len(listdir("./data/correct"))
 logs = LogParser("data/correct/log_original.txt").get_all_data()
-
-for position in positions_of_changes:
-    swap1 = logs.iloc[position, 1:4]
-    swap2 = logs.iloc[position+1, 1:4]
-
-    temp = swap1.copy()
-    logs.iloc[position, 1:4] = logs.iloc[position+1, 1:4]
-    logs.iloc[position+1, 1:4] = temp
-
-logs.to_csv(f'./data/correct/log{number_of_file}.txt', index=False, header=False)
-
-
+for i in range(0, 10):
+    create_logfile(logs)
 
 
 
