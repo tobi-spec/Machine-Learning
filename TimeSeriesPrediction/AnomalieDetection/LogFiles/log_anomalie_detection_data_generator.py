2f7ee29e05a38581e863626d973f495a6c6fe22c
Tobias Weiland <post@tobias-weiland.eu>
2024-07-18 20:55:17 +0200

    update data generation and visualisation

--- TimeSeriesPrediction/AnomalieDetection/LogFiles/log_anomalie_detection_data_generator.py
+++ TimeSeriesPrediction/AnomalieDetection/LogFiles/log_anomalie_detection_data_generator.py
@@ -8,24 +8,24 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 
 
-def create_logfile(logfile):
+def create_logfile():
+    logs = LogParser("data/correct/log_original.txt").get_all_data()
     number_of_file = len(listdir("./data/correct"))
     number_of_changes = np.random.randint(low=3, high=13)
     positions_of_changes = np.random.randint(low=1, high=49, size=number_of_changes)
     for position in positions_of_changes:
-        swap1 = logfile.iloc[position, 1:4]
-        swap2 = logfile.iloc[position+1, 1:4]
+        swap1 = logs.iloc[position, 1:4]
+        swap2 = logs.iloc[position+1, 1:4]
 
         temp = swap1.copy()
-        logfile.iloc[position, 1:4] = swap2
-        logfile.iloc[position+1, 1:4] = temp
+        logs.iloc[position, 1:4] = swap2
+        logs.iloc[position+1, 1:4] = temp
 
-    logfile.to_csv(f'./data/correct/log{number_of_file}.txt', index=False, header=False)
+    logs.to_csv(f'./data/correct/log{number_of_file}.txt', index=False, header=False)
 
 
-logs = LogParser("data/correct/log_original.txt").get_all_data()
-for i in range(0, 10):
-    create_logfile(logs)
+for i in range(0, 20):
+    create_logfile()
 
 
 
--- TimeSeriesPrediction/AnomalieDetection/LogFiles/log_anomalie_detection_visual.py
+++ TimeSeriesPrediction/AnomalieDetection/LogFiles/log_anomalie_detection_visual.py
@@ -11,13 +11,14 @@ def main():
     dataset_2 = DataBuilder("data/correct/log2.txt", translation_table).add_number_representation()
     dataset_3 = DataBuilder("data/correct/log3.txt", translation_table).add_number_representation()
     dataset_4 = DataBuilder("data/correct/log4.txt", translation_table).add_number_representation()
-
+    dataset_5 = DataBuilder("data/error/log_error.txt", translation_table).add_number_representation()
 
     fig, ax = plt.subplots(3, 2)
     ax[0, 0].plot(dataset_1.get_numbers())
     ax[0, 1].plot(dataset_2.get_numbers())
     ax[1, 0].plot(dataset_3.get_numbers())
     ax[1, 1].plot(dataset_4.get_numbers())
+    ax[2, 0].plot(dataset_5.get_numbers())
     plt.show()
 
 
