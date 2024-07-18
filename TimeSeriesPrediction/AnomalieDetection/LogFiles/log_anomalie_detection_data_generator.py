11daa31d9de9b61fca6cfe83b7467db1782cec11
Tobias Weiland <post@tobias-weiland.eu>
2024-07-18 20:20:01 +0200

    implement log_anomalie_detection_data_generator

---
+++ TimeSeriesPrediction/AnomalieDetection/LogFiles/log_anomalie_detection_data_generator.py
@@ -0,0 +1,31 @@
+from os import listdir
+from TimeSeriesPrediction.AnomalieDetection.LogFiles.log_anomalie_detection_keras_utils import LogParser
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+
+number_of_changes = np.random.randint(low=3, high=13)
+positions_of_changes = np.random.randint(low=1, high=51, size=number_of_changes)
+
+
+number_of_file = len(listdir("./data/correct"))
+logs = LogParser("data/correct/log_original.txt").get_all_data()
+
+for position in positions_of_changes:
+    swap1 = logs.iloc[position, 1:4]
+    swap2 = logs.iloc[position+1, 1:4]
+
+    temp = swap1.copy()
+    logs.iloc[position, 1:4] = logs.iloc[position+1, 1:4]
+    logs.iloc[position+1, 1:4] = temp
+
+logs.to_csv(f'./data/correct/log{number_of_file}.txt', index=False, header=False)
+
+
+
+
+
+
