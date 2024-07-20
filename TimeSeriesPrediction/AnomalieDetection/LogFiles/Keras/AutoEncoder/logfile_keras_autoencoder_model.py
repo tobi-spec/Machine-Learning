c8c0ad45ff4d22fb73ef919498dd545df1648751
Tobias Weiland <post@tobias-weiland.eu>
2024-07-20 13:26:45 +0200

    add Glorot initializer to model

--- TimeSeriesPrediction/AnomalieDetection/LogFiles/Keras/AutoEncoder/logfile_keras_autoencoder_model.py
+++ TimeSeriesPrediction/AnomalieDetection/LogFiles/Keras/AutoEncoder/logfile_keras_autoencoder_model.py
@@ -1,16 +1,24 @@
-from keras import Model, layers, Sequential
+from keras import Model, layers, Sequential, initializers
 
 
 class AutoEncoder(Model):
     def __init__(self):
         super(AutoEncoder, self).__init__()
         self.encoder = Sequential([
-            layers.Dense(50, activation="linear"),
-            layers.Dense(50, activation="linear")])
+            layers.Dense(50, activation="linear",
+                         kernel_initializer=initializers.GlorotNormal(),
+                         bias_initializer="zeros"),
+            layers.Dense(50, activation="linear",
+                         kernel_initializer=initializers.GlorotNormal(),
+                         bias_initializer="zeros")])
 
         self.decoder = Sequential([
-            layers.Dense(50, activation="linear"),
-            layers.Dense(50, activation="linear")])
+            layers.Dense(50, activation="linear",
+                         kernel_initializer=initializers.GlorotNormal(),
+                         bias_initializer="zeros"),
+            layers.Dense(50, activation="linear",
+                         kernel_initializer=initializers.GlorotNormal(),
+                         bias_initializer="zeros")])
 
     def call(self, x):
         encoded = self.encoder(x)
