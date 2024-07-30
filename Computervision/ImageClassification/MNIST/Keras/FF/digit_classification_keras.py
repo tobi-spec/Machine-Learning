dce217abfacb95c67feb577370b85cf34332b427
tobias.weiland <tobias.weiland@7p-group.com>
2024-07-30 22:02:55 +0200

    refactor digit_classification_keras_ff

--- Computervision/ImageClassification/MNIST/Keras/FF/digit_classification_keras.py
+++ Computervision/ImageClassification/MNIST/Keras/FF/digit_classification_keras.py
@@ -1,50 +1,43 @@
 import numpy as np
-import tensorflow as tf
+from keras import layers, Sequential, optimizers, losses
 import idx2numpy
 import matplotlib.pyplot as plt
-import timeit
+from yaml_parser import get_hyperparameters
 
-start = timeit.default_timer()
 
 class MNISTDataLoader:
     def __init__(self):
-        self.train_images = idx2numpy.convert_from_file("./data/train-images.idx3-ubyte")
-        self.train_labels = idx2numpy.convert_from_file("./data/train-labels.idx1-ubyte")
-        self.test_images = idx2numpy.convert_from_file("./data/t10k-images.idx3-ubyte")
-        self.test_labels = idx2numpy.convert_from_file("./data/t10k-labels.idx1-ubyte")
+        self.train_images: np.ndaray = idx2numpy.convert_from_file("../../data/train-images.idx3-ubyte").astype(
+            np.float32)
+        self.train_labels: np.ndaray = idx2numpy.convert_from_file("../../data/train-labels.idx1-ubyte")
+        self.test_images: np.ndaray = idx2numpy.convert_from_file("../../data/t10k-images.idx3-ubyte").astype(
+            np.float32)
+        self.test_labels: np.ndaray = idx2numpy.convert_from_file("../../data/t10k-labels.idx1-ubyte")
 
-    def info(self):
-        print("Number of images for training: ", len(self.train_images), self.train_images.shape)
-        print("Number of labels for training: ", len(self.train_labels), self.train_labels.shape)
-        print("Number of images for testing: ", len(self.test_images), self.test_images.shape)
-        print("Number of labels for testing: ", len(self.test_labels), self.test_labels.shape)
-
-    def display_train_image(self, position):
+    def display_train_image(self, position: int) -> None:
         plt.imshow(self.train_images[position], cmap=plt.cm.binary)
         plt.show()
 
-    def display_test_image(self, position):
+    def display_test_image(self, position: int) -> None:
         plt.imshow(self.test_images[position], cmap=plt.cm.binary)
         plt.show()
 
 
 mnist = MNISTDataLoader()
-mnist.info()
+hyperparameters: dict = get_hyperparameters("./digit_classification_keras_ff_hyperparameter.yaml")
 
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(units=128, activation="relu", kernel_initializer=tf.keras.initializers.RandomNormal(stddev=0.01), bias_initializer=tf.keras.initializers.Zeros()))
-model.add(tf.keras.layers.Dense(units=10, activation="softmax", kernel_initializer=tf.keras.initializers.RandomNormal(stddev=0.01), bias_initializer=tf.keras.initializers.Zeros()))
-model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss=tf.keras.losses.SparseCategoricalCrossentropy())
-model.fit(mnist.train_images, mnist.train_labels, epochs = 15, batch_size=32)
+model = Sequential()
+model.add(layers.Flatten(input_shape=(28, 28)))
+model.add(layers.Dense(units=128, activation="relu"))
+model.add(layers.Dense(units=10, activation="softmax"))
 
-results = model.evaluate(mnist.test_images, mnist.test_labels, batch_size=32)
-print("____________________________")
-print("validation: ", results)
-print("____________________________")
+model.compile(optimizer=optimizers.Adam(hyperparameters["learning_rate"]), loss=losses.SparseCategoricalCrossentropy())
+model.fit(mnist.train_images, mnist.train_labels, epochs=hyperparameters["epochs"],
+          batch_size=hyperparameters["batch_size"])
 
-prediction = model.predict(mnist.test_images[6758:6759])
-stop = timeit.default_timer()
-print("predicted number: ", np.argmax(prediction))
-print("correct number ",mnist.test_labels[6758:6759])
-print(f"run time[s]: {stop-start}")
+random_images = np.random.randint(low=0, high=10000, size=10)
+
+for image_index in random_images:
+    prediction: list = model.predict(mnist.test_images[[image_index]])  # add dimension for correct input shape
+    print("correct number ", mnist.test_labels[image_index])
+    print("predicted number: ", np.argmax(prediction))
