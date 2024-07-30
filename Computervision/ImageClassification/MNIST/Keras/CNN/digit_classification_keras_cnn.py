367ee52bdd09e7b136987469e5c6a129284cda93
tobias.weiland <tobias.weiland@7p-group.com>
2024-07-30 21:52:00 +0200

    refactor digit_classification_keras_cnn

--- Computervision/ImageClassification/MNIST/Keras/CNN/digit_classification_keras_cnn.py
+++ Computervision/ImageClassification/MNIST/Keras/CNN/digit_classification_keras_cnn.py
@@ -7,9 +7,11 @@ from yaml_parser import get_hyperparameters
 
 class MNISTDataLoader:
     def __init__(self):
-        self.train_images: np.ndaray = idx2numpy.convert_from_file("../../data/train-images.idx3-ubyte").astype(np.float32)
+        self.train_images: np.ndaray = idx2numpy.convert_from_file("../../data/train-images.idx3-ubyte").astype(
+            np.float32)
         self.train_labels: np.ndaray = idx2numpy.convert_from_file("../../data/train-labels.idx1-ubyte")
-        self.test_images: np.ndaray = idx2numpy.convert_from_file("../../data/t10k-images.idx3-ubyte").astype(np.float32)
+        self.test_images: np.ndaray = idx2numpy.convert_from_file("../../data/t10k-images.idx3-ubyte").astype(
+            np.float32)
         self.test_labels: np.ndaray = idx2numpy.convert_from_file("../../data/t10k-labels.idx1-ubyte")
 
     def display_train_image(self, position: int) -> None:
@@ -32,13 +34,16 @@ model.add(layers.Flatten(input_shape=(28, 28)))
 model.add(layers.Dense(units=128, activation="relu"))
 model.add(layers.Dense(units=10, activation="softmax"))
 
-
 model.compile(optimizer=optimizers.Adam(hyperparameters["learning_rate"]), loss=losses.SparseCategoricalCrossentropy())
-model.fit(mnist.train_images, mnist.train_labels, epochs=hyperparameters["epochs"], batch_size=hyperparameters["batch_size"])
+model.fit(mnist.train_images, mnist.train_labels, epochs=hyperparameters["epochs"],
+          batch_size=hyperparameters["batch_size"])
+
+random_images = np.random.randint(low=0, high=10000, size=10)
+
+for image_index in random_images:
+    prediction: list = model.predict(mnist.test_images[[image_index]])  # add dimension for correct input shape
+    print("correct number ", mnist.test_labels[image_index])
+    print("predicted number: ", np.argmax(prediction))
 
 
-
-prediction: list = model.predict(mnist.test_images[[6758]])
-print("predicted number: ", np.argmax(prediction))
-print("correct number ", mnist.test_labels[[6758]])
 
