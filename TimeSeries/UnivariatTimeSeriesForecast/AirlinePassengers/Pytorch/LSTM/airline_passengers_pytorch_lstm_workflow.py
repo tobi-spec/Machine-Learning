3fc2d00e47d9a104b3e78d0f6c2afb18f8aac83c
Tobias Weiland <post@tobias-weiland.eu>
2024-08-02 21:42:15 +0200

    refactor airline_passengers_pytorch_lstm

--- TimeSeries/UnivariatTimeSeriesForecast/AirlinePassengers/Pytorch/LSTM/airline_passengers_pytorch_lstm_workflow.py
+++ TimeSeries/UnivariatTimeSeriesForecast/AirlinePassengers/Pytorch/LSTM/airline_passengers_pytorch_lstm_workflow.py
@@ -6,15 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 from TimeSeries.UnivariatTimeSeriesForecast.AirlinePassengers.airline_passengers_utilities import \
-    AirlinePassengersDataSet, TimeSeriesGenerator
-
-EPOCHS = 100
-LEARNING_RATE = 0.001
-BATCH_SIZE = 1
-LOOK_BACK = 30
-LOOK_OUT = 1
-PREDICTION_START = -1
-NUMBER_OF_PREDICTIONS = 80
+    AirlinePassengersDataSet, TimeSeriesGenerator, plot_results
+from yaml_parser import get_hyperparameters
 
 
 def workflow(model):
@@ -25,8 +18,12 @@ def workflow(model):
     test_scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_test = test_scaler.fit_transform(airline_passengers.get_test_data().reshape(-1, 1))
 
-    train_timeseries, train_targets = TimeSeriesGenerator(scaled_train, LOOK_BACK, LOOK_OUT).create_timeseries()
-    test_timeseries, test_targets = TimeSeriesGenerator(scaled_test, LOOK_BACK, LOOK_OUT).create_timeseries()
+    hyperparameters: dict = get_hyperparameters("airline_passengers_pytorch_lstm_hyperparameter.yaml")
+
+    train_timeseries, train_targets = TimeSeriesGenerator(scaled_train, hyperparameters["look_back"],
+                                                          hyperparameters["look_out"]).create_timeseries()
+    test_timeseries, test_targets = TimeSeriesGenerator(scaled_test, hyperparameters["look_back"],
+                                                          hyperparameters["look_out"]).create_timeseries()
 
     train_timeseries = train_timeseries.reshape(train_timeseries.shape[0],train_timeseries.shape[2], train_timeseries.shape[1])
     test_timeseries = test_timeseries.reshape(test_timeseries.shape[0], test_timeseries.shape[2], test_timeseries.shape[1])
@@ -36,42 +33,30 @@ def workflow(model):
     test_timeseries_tensor = torch.tensor(test_timeseries, dtype=torch.float)
     test_targets_tensor = torch.tensor(test_targets, dtype=torch.float)
 
-    train_loader = DataLoader(dataset=TensorDataset(train_timeseries_tensor, train_targets_tensor), batch_size=BATCH_SIZE, shuffle=False)
-    test_loader = DataLoader(dataset=TensorDataset(test_timeseries_tensor, test_targets_tensor), batch_size=BATCH_SIZE, shuffle=False)
+    train_loader = DataLoader(dataset=TensorDataset(train_timeseries_tensor, train_targets_tensor), batch_size=hyperparameters["batch_size"], shuffle=False)
+    test_loader = DataLoader(dataset=TensorDataset(test_timeseries_tensor, test_targets_tensor), batch_size=hyperparameters["batch_size"], shuffle=False)
 
-    num_epochs = EPOCHS
-    for epoch in range(num_epochs):
-        model.backward(train_loader, epoch, num_epochs)
+    for epoch in range(hyperparameters["epochs"]):
+        model.backward(train_loader, epoch, hyperparameters["epochs"])
         model.validate(test_loader)
 
     validation_results = validation_forecast(model, test_timeseries_tensor)
 
     validation = pd.DataFrame()
     validation["validation"] = test_scaler.inverse_transform([validation_results]).flatten()
-    validation.index += airline_passengers.threshold + LOOK_BACK
+    validation.index += airline_passengers.threshold + hyperparameters["look_back"]
 
-    start_index = PREDICTION_START
+    start_index = -1
     start_value = train_timeseries_tensor[start_index]
     start_value_reshaped = start_value.reshape(1, start_value.shape[0], start_value.shape[1])
-    number_of_predictions = NUMBER_OF_PREDICTIONS
+    number_of_predictions = hyperparameters["number_of_predictions"]
     prediction_results = one_step_ahead_forecast(model, start_value_reshaped, number_of_predictions)
 
     prediction = pd.DataFrame()
     prediction["one_step_prediction"] = train_scaler.inverse_transform([prediction_results]).flatten()
     prediction.index += airline_passengers.threshold + start_index
 
-    plt.plot(airline_passengers.data["Passengers"], color="red", label="dataset")
-    plt.plot(airline_passengers.get_train_data(), color="green", label="training")
-    plt.plot(validation["validation"], color="blue", label="validation")
-    plt.plot(prediction["one_step_prediction"], color="orange", label="one_step_prediction")
-    plt.title("airline passengers prediction LSTM")
-    plt.xlabel("Time[Month]")
-    plt.ylabel("Passengers[x1000]")
-    plt.xticks(range(0, 200, 20))
-    plt.yticks(range(0, 1000, 100))
-    plt.legend(loc="upper left")
-    plt.savefig("./airlinePassengers_pytorch_lstm.png")
-    plt.show()
+    plot_results(prediction["one_step_prediction"], validation["validation"], hyperparameters["name"])
 
 
 def validation_forecast(model, inputs):
