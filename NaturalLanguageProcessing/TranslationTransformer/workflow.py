4b52f0c050ca4495116020f79d456beb06c5d775
tobias.weiland <tobias.weiland@7p-group.com>
2025-02-01 15:36:00 +0100

    refactore transformer

--- NaturalLanguageProcessing/TranslationTransformer/workflow.py
+++ NaturalLanguageProcessing/TranslationTransformer/workflow.py
@@ -59,54 +59,9 @@ transformer = Transformer(
     dropout_rate=hyperparameters["dropout_rate"])
 
 
-class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, d_model, warmup_steps=4000):
-        super().__init__()
-
-        self.d_model = d_model
-        self.d_model = tf.cast(self.d_model, tf.float32)
-
-        self.warmup_steps = warmup_steps
-
-    def __call__(self, step):
-        step = tf.cast(step, dtype=tf.float32)
-        arg1 = tf.math.rsqrt(step)
-        arg2 = step * (self.warmup_steps ** -1.5)
-
-        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
-
-
 learning_rate = CustomSchedule(hyperparameters["d_model"])
 optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
                                      epsilon=1e-9)
-
-
-def masked_loss(label, pred):
-    mask = label != 0
-    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction='none')
-    loss = loss_object(label, pred)
-
-    mask = tf.cast(mask, dtype=loss.dtype)
-    loss *= mask
-
-    loss = tf.reduce_sum(loss) / tf.reduce_sum(mask)
-    return loss
-
-
-def masked_accuracy(label, pred):
-    pred = tf.argmax(pred, axis=2)
-    label = tf.cast(label, pred.dtype)
-    match = label == pred
-
-    mask = label != 0
-
-    match = match & mask
-
-    match = tf.cast(match, dtype=tf.float32)
-    mask = tf.cast(mask, dtype=tf.float32)
-    return tf.reduce_sum(match) / tf.reduce_sum(mask)
-
 
 transformer.compile(
     loss=masked_loss,
