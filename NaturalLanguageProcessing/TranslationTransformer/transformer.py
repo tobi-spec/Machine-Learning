1c484423406230af346ed3df485dae7dff4bbd3a
tobias.weiland <tobias.weiland@7p-group.com>
2025-02-01 15:28:46 +0100

    move hyperparameters into yaml file

--- NaturalLanguageProcessing/TranslationTransformer/transformer.py
+++ NaturalLanguageProcessing/TranslationTransformer/transformer.py
@@ -3,8 +3,10 @@ import tensorflow_datasets as tfds
 import tensorflow as tf
 import tensorflow_text
 from utils import *
+from yaml_parser import get_hyperparameters
 
 setup_gpu_usage()
+hyperparameters: dict = get_hyperparameters("hyperparameters.yaml")
 
 examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
                                with_info=True,
@@ -19,31 +21,25 @@ tf.keras.utils.get_file(
 )
 tokenizers = tf.saved_model.load(model_name)
 
-MAX_TOKENS = 128
-
 
 def prepare_batch(pt, en):
     pt = tokenizers.pt.tokenize(pt)  # Output is ragged.
-    pt = pt[:, :MAX_TOKENS]  # Trim to MAX_TOKENS.
+    pt = pt[:, :hyperparameters["max_tokens"]]  # Trim to MAX_TOKENS.
     pt = pt.to_tensor()  # Convert to 0-padded dense Tensor
 
     en = tokenizers.en.tokenize(en)
-    en = en[:, :(MAX_TOKENS + 1)]
+    en = en[:, :(hyperparameters["max_tokens"] + 1)]
     en_inputs = en[:, :-1].to_tensor()  # Drop the [END] tokens
     en_labels = en[:, 1:].to_tensor()  # Drop the [START] tokens
 
     return (pt, en_inputs), en_labels
 
 
-BUFFER_SIZE = 20000
-BATCH_SIZE = 32
-
-
 def make_batches(dataset):
     return (
         dataset
-            .shuffle(BUFFER_SIZE)
-            .batch(BATCH_SIZE)
+            .shuffle(hyperparameters["buffer_size"])
+            .batch(hyperparameters["batch_size"])
             .map(prepare_batch, tf.data.AUTOTUNE)
             .prefetch(buffer_size=tf.data.AUTOTUNE)
     )
@@ -304,20 +300,15 @@ class Transformer(tf.keras.Model):
         return logits
 
 
-num_layers = 1
-d_model = 128
-dff = 512
-num_heads = 8
-dropout_rate = 0.1
 
 transformer = Transformer(
-    num_layers=num_layers,
-    d_model=d_model,
-    num_heads=num_heads,
-    dff=dff,
+    num_layers=hyperparameters["number_of_layers"],
+    d_model=hyperparameters["d_model"],
+    num_heads=hyperparameters["number_of_heads"],
+    dff=hyperparameters["dff"],
     input_vocab_size=tokenizers.pt.get_vocab_size().numpy(),
     target_vocab_size=tokenizers.en.get_vocab_size().numpy(),
-    dropout_rate=dropout_rate)
+    dropout_rate=hyperparameters["dropout_rate"])
 
 output = transformer((pt, en))
 
@@ -339,7 +330,7 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 
-learning_rate = CustomSchedule(d_model)
+learning_rate = CustomSchedule(hyperparameters["d_model"])
 optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
                                      epsilon=1e-9)
 
