3f6e07141e7bcf9745af9948c66cb2f3aedae2da
Tobias Weiland <tobias.weiland@7p-group.com>
2025-09-12 11:39:50 +0200

    add rag to ollama with all

--- generativAI/text_generation/llm_with_all/ollama_with_all.py
+++ generativAI/text_generation/llm_with_all/ollama_with_all.py
@@ -1,24 +1,44 @@
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from operator import itemgetter
 from langchain_community.vectorstores import FAISS
 from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.documents import Document
-from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, \
-    MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableWithMessageHistory, RunnableConfig
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_ollama import ChatOllama
 
 
+documents = [
+    Document(page_content="Kevin likes ice cream."),
+    Document(page_content="Kevin dont likes cookies"),
+]
+
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+vector_store = FAISS.from_documents(documents, embedding_model)
+retriever = vector_store.as_retriever()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+
+
 prompt = ChatPromptTemplate.from_messages([
-    SystemMessagePromptTemplate.from_template("You are a helpful assistant."),
+    SystemMessagePromptTemplate.from_template(
+        "You are a helpful assistant. Use the following context when relevant:\n\n{context}"
+    ),
     MessagesPlaceholder(variable_name="history"),
     HumanMessagePromptTemplate.from_template("{input}")
 ])
 
+
 model = ChatOllama(model="mistral")
 
-chain = prompt | model
+retrieval_chain = {
+    "context": itemgetter("input") | retriever | format_docs,
+    "input": lambda x: x["input"],
+    "history": lambda x: x["history"]
+} | prompt | model
 
 
 
@@ -29,13 +49,15 @@ def get_session_history(session_id: str):
     return store[session_id]
 
 chain_with_history = RunnableWithMessageHistory(
-    runnable=chain,
+    runnable=retrieval_chain,
     get_session_history=get_session_history,
     input_messages_key="input",
     history_messages_key="history"
 )
 
 config: RunnableConfig = {"configurable": {"session_id": "session1"}}
+
+
 
 print("Hello! My name is Charlie")
 for chunk in chain_with_history.stream({"input": "Hello! My name is Charlie"}, config=config):
@@ -49,4 +71,10 @@ for chunk in chain_with_history.stream({"input": "What's my name?"}, config=conf
     if chunk:
         print(chunk.content, end="", flush=True)
 
+print("\n")
+
+print("What does Kevin like?")
+for chunk in chain_with_history.stream({"input": "What does Kevin like?"}, config=config):
+    if chunk:
+        print(chunk.content, end="", flush=True)
 
