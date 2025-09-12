dce836a715cddbf84de4a3cc95875854fe6c92c0
Tobias Weiland <tobias.weiland@7p-group.com>
2025-09-12 10:14:35 +0200

    implement example with templates

---
+++ generativAI/text_generation/llm_with_prompt_template/ollama_with_prompt_template.py
@@ -0,0 +1,27 @@
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+
+from langchain_ollama import ChatOllama
+
+from langchain.schema import HumanMessage, AIMessage, SystemMessage
+
+
+humane_message = HumanMessage(content="I am a human message")
+print(humane_message)
+
+ai_message = AIMessage(content="I am a ai message")
+print(ai_message)
+
+system_message = SystemMessage(content="I am a system message")
+print(system_message)
+
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template("You are a helpful assistant."),
+    HumanMessagePromptTemplate.from_template("Tell my a joke about {input}")]
+)
+
+model = ChatOllama(model="mistral")
+
+chain = prompt.pipe(model) # underlies prompt | model
+
+answer = chain.invoke({"input": "chickens"})
+print(answer.content)
