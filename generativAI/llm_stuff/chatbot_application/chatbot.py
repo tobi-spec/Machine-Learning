bed1c70402f53f8bd8ceb850eafad62422c142cd
Tobias Weiland <tobias.weiland@7p-group.com>
2025-12-17 11:50:12 +0100

    add simple chatbot example

---
+++ generativAI/llm_stuff/chatbot_application/chatbot.py
@@ -0,0 +1,40 @@
+import random
+from time import sleep
+import streamlit as st
+import numpy as np
+from langchain_ollama import ChatOllama
+
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        sleep(0.05)
+
+model = ChatOllama(model="mistral")
+
+st.title("Chatbot Application")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Ask me anything!")
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("asssistant"):
+        response = model.invoke(prompt)
+        st.markdown(response.content)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
