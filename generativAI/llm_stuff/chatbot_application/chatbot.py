14942e2bf74cac72850cc5b9cedb3d7b483e1af5
Tobias Weiland <tobias.weiland@7p-group.com>
2025-12-17 11:50:46 +0100

    clean u´p chatbot example

--- generativAI/llm_stuff/chatbot_application/chatbot.py
+++ generativAI/llm_stuff/chatbot_application/chatbot.py
@@ -1,20 +1,5 @@
-import random
-from time import sleep
 import streamlit as st
-import numpy as np
 from langchain_ollama import ChatOllama
-
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        sleep(0.05)
 
 model = ChatOllama(model="mistral")
 
