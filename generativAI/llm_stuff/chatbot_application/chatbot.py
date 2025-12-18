d601bbc1f1e75be185c4eaec0cea055e0f30dd36
Tobias Weiland <post@tobias-weiland.eu>
2025-12-18 21:25:30 +0100

    add history to chatbot

--- generativAI/llm_stuff/chatbot_application/chatbot.py
+++ generativAI/llm_stuff/chatbot_application/chatbot.py
@@ -1,7 +1,40 @@
 import streamlit as st
+from langchain_core.chat_history import InMemoryChatMessageHistory, BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder, \
+    HumanMessagePromptTemplate
+from langchain_core.runnables import RunnableWithMessageHistory, Runnable, RunnableConfig
 from langchain_ollama import ChatOllama
 
-model = ChatOllama(model="mistral")
+model: Runnable = ChatOllama(model="mistral")
+
+prompt: Runnable = ChatPromptTemplate.from_messages([
+    MessagesPlaceholder(variable_name="history"),
+    HumanMessagePromptTemplate.from_template("{input}")
+])
+
+chain: Runnable = prompt | model
+
+if "lc_store" not in st.session_state:
+    st.session_state["lc_store"] = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    store = st.session_state["lc_store"]
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+chain_with_history = RunnableWithMessageHistory(
+    runnable=chain,
+    get_session_history=get_session_history,
+    input_messages_key="input",
+    history_messages_key="history"
+)
+
+session_id = "session1"
+config: RunnableConfig = {"configurable": {"session_id": session_id}}
+history = get_session_history(session_id)
+st.write(history)
+
 
 st.title("Chatbot Application")
 
@@ -12,18 +45,21 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-prompt = st.chat_input("Ask me anything!")
-if prompt:
+st.write(st.session_state.messages)
+
+user_input = st.chat_input("Ask me anything!")
+if user_input:
     with st.chat_message("user"):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        st.markdown(user_input)
+    st.session_state.messages.append({"role": "user", "content": user_input})
 
     with st.chat_message("assistant"):
         response_placeholder = st.empty()
         full_response = ""
-        for chunk in model.stream(prompt):
+        for chunk in chain_with_history.stream(input={"input": user_input}, config=config):
             full_response += chunk.content
             response_placeholder.markdown(full_response + "▌")
         response_placeholder.markdown(full_response)
+
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
