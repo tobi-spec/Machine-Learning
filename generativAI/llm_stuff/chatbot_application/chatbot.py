d692baaba6a13d5e0732191615a4e3627c1aba22
Tobias Weiland <post@tobias-weiland.eu>
2025-12-21 08:48:40 +0100

    add simple document digest

--- generativAI/llm_stuff/chatbot_application/chatbot.py
+++ generativAI/llm_stuff/chatbot_application/chatbot.py
@@ -1,4 +1,9 @@
+import os
+from io import BytesIO
+
 import streamlit as st
+from docling.document_converter import DocumentConverter
+from docling_core.types.io import DocumentStream
 from langchain_core.chat_history import InMemoryChatMessageHistory, BaseChatMessageHistory
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder, \
@@ -36,7 +41,6 @@ config: RunnableConfig = {"configurable": {"session_id": session_id}}
 
 
 st.title("Chatbot Application")
-
 history = get_session_history(session_id)
 for message in history.messages:
     if isinstance(message, HumanMessage):
@@ -45,7 +49,6 @@ for message in history.messages:
     else:
         with st.chat_message("assistant"):
             st.markdown(message.content)
-
 
 user_input = st.chat_input("Ask me anything!")
 if user_input:
@@ -59,4 +62,15 @@ if user_input:
             full_response += chunk.content
             response_placeholder.markdown(full_response + "▌")
         response_placeholder.markdown(full_response)
+
+with st.sidebar:
+    uploaded_file = st.file_uploader(label="Add to RAG", type=["pdf", "docx", "csv"])
+
+    if uploaded_file is not None:
+        buf = BytesIO(uploaded_file.getvalue())
+        source = DocumentStream(name=uploaded_file.name, stream=buf)
+        converter = DocumentConverter()
+        result = converter.convert(source)
+        markdown = result.document.export_to_markdown()
+        st.markdown(markdown)
 
