f95854d312c00047deca2bc73e104be3dbd8438d
Tobias Weiland <tobias.weiland@7p-group.com>
2026-01-21 09:37:13 +0100

    add comment to chatbot.py

--- generativAI/llm_stuff/chatbot_application/chatbot.py
+++ generativAI/llm_stuff/chatbot_application/chatbot.py
@@ -20,8 +20,16 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_ollama import ChatOllama
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# TODOS:
-# ragas tests
+
+'''
+langchain, at its base, creates a dict which is passed through the chain and get altered by the different components.
+Following keys are added during the process:
+{
+    "context": {context},
+    "input": {input},
+    "history": {history}
+}
+'''
 
 if "vectordb" not in st.session_state:
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
