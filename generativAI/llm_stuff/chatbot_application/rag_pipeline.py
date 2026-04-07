f2203dc963abbc96be4b16f76d011b2a0b59b32c
Tobias Weiland <tobias.weiland@7p-group.com>
2026-04-07 16:40:51 +0200

    add bm25 to rag pipeline

--- generativAI/llm_stuff/chatbot_application/rag_pipeline.py
+++ generativAI/llm_stuff/chatbot_application/rag_pipeline.py
@@ -3,6 +3,8 @@ from uuid import uuid4
 
 from docling.document_converter import DocumentConverter
 from docling_core.types.io import DocumentStream
+from langchain_classic.retrievers import EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever
 from langchain_core.documents import Document
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -11,9 +13,22 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 class RAGPipeline:
     def __init__(self, database) -> None:
         self.database = database
+        self.documents: list = []
 
-    def get_retriever(self) -> VectorStoreRetriever:
+    def get_retriever(self):
+        if not self.documents:
+            return self._get_vectores()
+
+        return EnsembleRetriever(
+            retrievers=[self._get_vectores(), self._get_bm25()],
+            weights=[0.6, 0.4]
+        )
+
+    def _get_vectores(self) -> VectorStoreRetriever:
         return self.database.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+
+    def _get_bm25(self):
+        return BM25Retriever.from_documents(self.documents, k=12)
 
     def digest(self, uploaded_file) -> None:
         buf = BytesIO(uploaded_file.getvalue())
@@ -31,5 +46,6 @@ class RAGPipeline:
         ])
         print(f"Indexed {len(docs)} chunks from {uploaded_file.name}")
 
+        self.documents = docs
         ids = [str(uuid4()) for _ in range(len(docs))]
         self.database.add_documents(docs, ids=ids)
