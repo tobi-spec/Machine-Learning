08e58fc2dae0ba2c1dc328abe3d69f5b4fdc9f53
Tobias Weiland <tobias.weiland@7p-group.com>
2026-07-31 15:45:19 +0200

    use mnist example in architecture

--- 03_computer_vision/01_architectures/data/digit_classification_visual.py
+++ 03_computer_vision/01_architectures/data/digit_classification_visual.py
@@ -5,12 +5,14 @@ from matplotlib import pyplot as plt
 
 class MNISTDataLoader:
     def __init__(self):
-        self.train_images: np.ndaray = idx2numpy.convert_from_file("data/train-images.idx3-ubyte").astype(
+        self.train_images: np.ndaray = idx2numpy.convert_from_file(
+            "train-images.idx3-ubyte").astype(
             np.float32)
-        self.train_labels: np.ndaray = idx2numpy.convert_from_file("data/train-labels.idx1-ubyte")
-        self.test_images: np.ndaray = idx2numpy.convert_from_file("data/t10k-images.idx3-ubyte").astype(
+        self.train_labels: np.ndaray = idx2numpy.convert_from_file(
+            "train-labels.idx1-ubyte")
+        self.test_images: np.ndaray = idx2numpy.convert_from_file("t10k-images.idx3-ubyte").astype(
             np.float32)
-        self.test_labels: np.ndaray = idx2numpy.convert_from_file("data/t10k-labels.idx1-ubyte")
+        self.test_labels: np.ndaray = idx2numpy.convert_from_file("t10k-labels.idx1-ubyte")
 
     def display_train_image(self, position: int) -> None:
         plt.imshow(self.train_images[position], cmap=plt.cm.binary)
